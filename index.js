047939903ce79a27d461e93503475ee425e83abe
Devastated God <75840562+DevastatedGod@users.noreply.github.com>
2020-12-18 15:05:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://trail-inshare.herokuapp.com";
+const baseURL = "https://devastatedgod.github.io/trail";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
